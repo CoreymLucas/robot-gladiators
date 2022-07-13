cb8e9f294e1ed7d99704550ceb2f03bde458065a
Corey Lucas <coreymlucas@icloud.com>
2022-07-12 18:00:12 -0700

    added console log for player name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,19 @@
-window.alert("This is an alert! JavaScript is running");
+var playerName = window.prompt("What is your robots name?");
+console.log(playerName);
+
+// this will do math and log 20 //
+console.log(10+10);
+console.log("Our robots name is" + playerName)
+
+
+
+
+
+// this creates a function name "fight"
+function fight() 
+{
+    window.alert("The fight has begun!");
+}
+
+
+// fight();
